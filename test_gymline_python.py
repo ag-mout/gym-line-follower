c4458125b72e403f4675cd56bff4297044acb17f
andre <andregmoutinho@gmail.com>
2025-08-19 17:05:25 +0100

    Add RenderCollection wrapper and set seed=123

--- test_gymline_python.py
+++ test_gymline_python.py
@@ -1,25 +1,32 @@
 #test gym_line_follower package
 import gym_line_follower
 import gymnasium as gym
+from gymnasium.wrappers.rendering import RenderCollection
 import matplotlib.pyplot as plt
-from stable_baselines3.common.env_checker import check_env
+# from stable_baselines3.common.env_checker import check_env
 import imageio
 
 
 env = gym.make('LineFollower-v0', gui = True, render_mode = 'rgb_array')
+env = RenderCollection(env)
 # check_env(env, warn=True, skip_render_check=False)
-env.reset()
+
+env.reset(seed=123)
+
 frames = []
-for i in range(1000):
+for i in range(100):
     action = env.action_space.sample()
     obsv, reward, done, truncated, info = env.step(action)
-    # print("Reward: ", reward)
-    frames.append(env.render())
+    print("Reward: ", reward)
+    
     if done:
         break
-env.render()
+
+frames = env.render()
 print(obsv)
 print("Done: ", done)
+
 imageio.mimsave('test.gif', frames)
+
 #close the environment
 env.close()
